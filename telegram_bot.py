8f6ef464aec1fb759e3ce94372c2b882cbf433d6
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2025-01-05 18:02:33 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,12 @@
 import os
 from pyrogram import Client, filters
+from pyrogram.storage import MemoryStorage
 
 api_id = 26107399
 api_hash = "e10525d8ad0189f8bf7a82a32f538d12"
 session_string = os.getenv("SESSION_STRING")
 
-app = Client(session_string, api_id=api_id, api_hash=api_hash)
+app = Client(session_string, api_id=api_id, api_hash=api_hash, storage=MemoryStorage())
 
 @app.on_message(filters.private)
 def handle_message(client, message):
